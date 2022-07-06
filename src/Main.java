98258e7faee66e85ef04c9fe47b4fe57893a2a4d
Aladyev Dmitriy <aladyev.dmitriy@gmail.com>
2022-07-06 15:30:21 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 2000;
+        int refill = 17000;
+        int bonusCost = 1;
+        int bonus;
+
+        if (refill > 1000) {
+            bonus = 1 * refill / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Бонус составил =" + (bonus));
+        System.out.println("Баланс=" + (balance + refill + bonus));
+    }
+}
